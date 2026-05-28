c3438c2c058af08c2b682fc3096113d9e3ecd9dd
Henrique <teteminglin@gmail.com>
2026-05-27 21:27:07 -0300

    BACKUP: Tem algo errado com o script2.js, não to conseguindo arrumar, terei que refazer

--- API2/script2.js
+++ API2/script2.js
@@ -1,45 +1,33 @@
-const url = "https://opentdb.com/api.php?amount=10&type=multiple";
+const diaSelect = document.getElementById("dia");
+const mesSelect = document.getElementById("mes");
 
+for (let dia = 1; dia <= 30; dia++) {
+  diaSelect.innerHTML += `<option value="${dia}">${dia}</option>`;
+}
+
+for (let mes = 1; mes <= 12; mes++) {
+  mesSelect.innerHTML += `<option value="${mes}">${mes}</option>`;
+}
+
+  function buscarCotacao() {
+
+  const dia = Number(diaSelect.value);
+  const mes = Number(mesSelect.value);
+
+  if (!dia || !mes) return;
+
+  const numero_dias = ((mes - 1) * 30) + dia;
+
+  const url = `https://economia.awesomeapi.com.br/json/daily/USD-BRL/${numero_dias}`;
+  console.log(url);
+  console.log(numero_dias);
+  
     fetch(url)
       .then(response => response.json())
       .then(data => {
         const div = document.getElementById("desgraca");
-
-        data.results.forEach(pergunta => {
-          div.innerHTML += `
-          <div id="subdiv">
-            <h2>${decodeURIComponent(pergunta.question)}</h2>
-            <button id="Y">${decodeURIComponent(pergunta.correct_answer)}</button>
-            <button id="Y">${decodeURIComponent(pergunta.incorrect_answers[0])}</button>
-            <button id="Y">${decodeURIComponent(pergunta.incorrect_answers[1])}</button>
-            <button id="Y">${decodeURIComponent(pergunta.incorrect_answers[2])}</button>
-            </div>
-            `;
-            
-        });
       })
       .catch(error => {
         console.error("Erro:", error);
       });
-
-
-      //ISSO É SÓ PRA SER LEGAL
-      BigInt.addEvebntListener('mouseenter', () => {
-        const x1 = Math.random() * 30 - 15;
-        const x2 = Math.random() * 30 - 15;
-
-        btn.style.animation = 'none';
-        btn.offsetHeight;
-        btn.style.animation = 'shake-style';
-
-        const style = document.getElementById('shake-style') || document.createElement('style');
-        style.id = 'shake-style';
-        style.textContent = `
-          @keyframes shake {
-            0% { transform: translateX(0); }
-            25% { transform: translateX(${x1}px); }
-            50% { transform: translateX(0); }
-            75% { transform: translateX(${x2}px); }
-            100% { transform: translateX(0); }
-        `;
-        document.head.appendChild(style);});
+    }
